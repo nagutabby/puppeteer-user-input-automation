fef6c59802a3d5ac5a29be45e4f054a3c8716ee1
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-12-29 06:05:50 +0900

    ズームインとズームアウトをシミュレートする

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false, args: [
+      "--force-device-scale-factor"
+    ]
+  });
   const page = await browser.newPage();
+  await page.goto("https://tourist-information.pages.dev/locations");
+  // moto g power
+  const width = 412;
+  const height = 823;
+  const deviceScaleFactor = 2;
+  await page.setViewport({
+    width: Math.floor(width / deviceScaleFactor), height: Math.floor(height / deviceScaleFactor), deviceScaleFactor: 2
+  });
 });
