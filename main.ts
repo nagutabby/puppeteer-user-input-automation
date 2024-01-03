c579cef733b3daf71b7957d2b9c1e8e635645fbc
nagutabby <62084485+nagutabby@users.noreply.github.com>
2024-01-04 08:42:40 +0900

    スクロールした際に読み込まれるリソースのメトリクスを表示する

--- main.ts
+++ main.ts
@@ -1,18 +1,33 @@
-import puppeteer from "puppeteer";
+import puppeteer, { Puppeteer, KnownDevices } from "puppeteer";
+import { setTimeout } from 'node:timers/promises';
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false, args: [
-      "--force-device-scale-factor"
-    ]
-  });
-  const page = await browser.newPage();
-  await page.goto("https://tourist-information.pages.dev/locations");
-  // moto g power
-  const width = 412;
-  const height = 823;
-  const deviceScaleFactor = 2;
-  await page.setViewport({
-    width: Math.floor(width / deviceScaleFactor), height: Math.floor(height / deviceScaleFactor), deviceScaleFactor: 2
-  });
+const browser = await puppeteer.launch({
+  headless: false
 });
+const page = await browser.newPage();
+
+await page.emulate(KnownDevices["Pixel 5"]);
+
+await page.goto("https://tourist-information.pages.dev/locations/北海道", { waitUntil: 'networkidle0' });
+
+await page.mouse.down();
+// 1ページ下にスクロールする
+await page.mouse.move(0, -851);
+await page.mouse.up();
+
+await setTimeout(5000);
+
+const rawResources: string = await page.evaluate(() => {
+  const resources = performance.getEntriesByType('resource');
+  return JSON.stringify(resources);
+});
+const resources: PerformanceEntryList = JSON.parse(rawResources);
+console.log(`読み込んだリソース数: ${resources.length}`);
+resources.forEach(resource => {
+  console.log(`redirect time: ${Math.floor(resource.redirectEnd - resource.redirectStart)}`);
+  console.log(`domain lookup time: ${Math.floor(resource.domainLookupEnd - resource.domainLookupStart)}`);
+  console.log(`connecting time: ${Math.floor(resource.connectEnd - resource.connectStart)}`);
+  console.log(`response time: ${resource.responseStart === 0 ? 0 : Math.floor(resource.responseEnd - resource.responseStart)}`);
+  console.log(`duration: ${Math.floor(resource.duration)}`);
+});
+await browser.close();
